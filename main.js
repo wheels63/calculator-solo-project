f02745ec7fa63dbe1aba1142bbcdf7b8dd782d61
Taeho Lee <ksltho@gmail.com>
2022-07-21 15:03:06 -0400

    first push for calculator project, create layout, add js functionality

---
+++ main.js
@@ -0,0 +1,59 @@
+let total = 0;
+let operator = "";
+
+// operators
+document.querySelector("#subtraction").addEventListener("click",minus)
+document.querySelector("#addition").addEventListener("click", addition)
+document.querySelector("#multiplication").addEventListener("click", multiplication)
+document.querySelector("#division").addEventListener("click", division)
+document.querySelector("#reset").addEventListener("click", reset)
+
+function minus() {
+  operator = "-"
+  document.querySelector("#placeToShowOperator").innerText = "-";
+  // document.querySelector("#placeToShow").innerText = total;
+}
+
+function addition() {
+  operator = "+";
+  document.querySelector("#placeToShowOperator").innerText = "+";
+}
+
+function multiplication() {
+  operator = "x";
+  document.querySelector("#placeToShowOperator").innerText = "x";
+}
+
+function division() {
+  operator = "/";
+  document.querySelector("#placeToShowOperator").innerText = "%";
+}
+function reset() {
+  operator = "";
+  total = 0;
+  document.querySelector("#placeToShow").innerText = "The calculator is reset";
+}
+
+// numbers
+document.querySelector("#one").addEventListener("click", one)
+document.querySelector("#two").addEventListener("click", two)
+document.querySelector("#three").addEventListener("click", three)
+document.querySelector("#four").addEventListener("click", four)
+document.querySelector("#five").addEventListener("click", five)
+document.querySelector("#six").addEventListener("click", six)
+document.querySelector("#seven").addEventListener("click", seven)
+document.querySelector("#eight").addEventListener("click", eight)
+document.querySelector("#nine").addEventListener("click", nine)
+
+function one() {
+  document.querySelector("#placeToShow").innerText = total;
+}
+function two() {
+  document.querySelector("#placeToShow").innerText = total;
+}
+function three() {
+  document.querySelector("#placeToShow").innerText = total;
+}
+
+// i click on button
+// if no operator is in place, and the total is zero. 
